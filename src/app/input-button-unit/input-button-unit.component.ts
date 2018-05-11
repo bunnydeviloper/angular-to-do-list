58f77f02f92ceda4a57b0c9867eddbea09292bd1
ThanhPhuong Mai <MaiAwesomeMac@ThanhPhuongs-MacBook-Pro.local>
2018-05-11 12:40:04 -0700

    add button event binding

--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -2,14 +2,33 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-input-button-unit',
-  templateUrl: './input-button-unit.component.html',
+  template: `
+    <p>
+      input-button-unit works!
+      The title is {{ title }}
+    </p>
+
+    <input [value]="title"
+      (keyup.enter)="changeTitle($event.target.value)"
+      >
+      <button (click)="changeTitle('Button Clicked!')">
+        Save
+      </button>
+  `,
   styleUrls: ['./input-button-unit.component.css']
 })
 export class InputButtonUnitComponent implements OnInit {
 
-  constructor() { }
+  title = 'Hello World!';
+
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  changeTitle(newTitle: string) {
+    this.title = newTitle;
   }
 
 }
