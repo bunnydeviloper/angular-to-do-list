c792e81b439193bb9943e76095fb5fcbef1ff2fb
ThanhPhuong Mai <MaiAwesomeMac@ThanhPhuongs-MacBook-Pro.local>
2018-05-11 13:20:53 -0700

    update list manager

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,27 +4,16 @@ import { TodoItem } from './interfaces/todo-item';
 @Component({
   selector: 'app-root',
   template: `
-    <h1 *ngIf="userLoggedIn">Welcome!</h1>
+  <h1>
+    Welcome to {{ title }}!
+  </h1>
 
-    <app-input-button-unit (submit)="addItem($event)">
-    </app-input-button-unit>
-    
-    <ul>
-      <li *ngFor="let todoItem of todoList">
-        {{ todoItem.title }}
-      </li>
-    </ul>
-
-    <ul>
-      <li *ngFor="let item of todoList">
-        <app-todo-item [item]="todoItem"></app-todo-item>
-      </li>
-    </ul>
+  <app-list-manager></app-list-manager>
   `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-title = 'app';
+title = 'My To-Do List APP';
 todoList: TodoItem[] = [
   {title: 'install NodeJS'},
   {title: 'install Angular CLI'},
--- src/app/list-manager/list-manager.component.ts
+++ src/app/list-manager/list-manager.component.ts
@@ -1,15 +1,37 @@
+import { TodoItem } from '../interfaces/todo-item';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-list-manager',
-  templateUrl: './list-manager.component.html',
+  template: `
+    <app-input-button-unit (submit)="addItem($event)"></app-input-button-unit>
+
+    <ul>
+      <li *ngFor="let todoItem of todoList">
+        <app-todo-item [item]="todoItem"></app-todo-item>
+      </li>
+    </ul>
+    `,
   styleUrls: ['./list-manager.component.css']
 })
 export class ListManagerComponent implements OnInit {
 
+  todoList: TodoItem[] = [
+    {title: 'install NodeJS'},
+    {title: 'install Angular CLI'},
+    {title: 'create new app'},
+    {title: 'serve app'},
+    {title: 'develop app'},
+    {title: 'deploy app'},
+  ];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addItem(title: string) {    
+    this.todoList.push({ title });
   }
 
 }
