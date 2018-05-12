6fb66ec4e9696b7a6c46b8148b9f734c356a3cc9
ThanhPhuong Mai <MaiAwesomeMac@thanhphuongsmbp.attlocal.net>
2018-05-12 00:40:25 -0700

    add remove button and strikethrough checkboxes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { AppComponent } from './app.component';
 import { InputButtonUnitComponent } from './input-button-unit/input-button-unit.component';
 import { TodoItemComponent } from './todo-item/todo-item.component';
 import { ListManagerComponent } from './list-manager/list-manager.component';
-import { StorageService } from './services/storage.service';
 
 @NgModule({
   declarations: [
