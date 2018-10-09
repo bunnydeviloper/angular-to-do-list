1212291761ea33ed0fe58fda48856648fe5ddfa9
Sophia Mai <phuongmai001@gmail.com>
2018-10-09 10:14:15 -0700

    add more styling to input element and buttons

--- todo-list/src/app/app.component.ts
+++ todo-list/src/app/app.component.ts
@@ -3,12 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <h1>Angular {{ title }}!</h1>
+    <h1 class="app-title">Angular {{ title }}!</h1>
 
     <app-list-manager></app-list-manager>
   `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Todo List App';
+  title = 'Todo List';
 }
