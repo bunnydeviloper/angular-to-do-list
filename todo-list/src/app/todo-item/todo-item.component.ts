b985a4fa4d39c9e1cc13f1c861605c33c581ce99
Sophia Mai <phuongmai001@gmail.com>
2018-10-09 09:29:19 -0700

    create todo-item component and bind data to it

--- todo-list/src/app/todo-item/todo-item.component.ts
+++ todo-list/src/app/todo-item/todo-item.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-todo-item',
-  templateUrl: './todo-item.component.html',
+  template: `
+    {{ item.title }}
+    `,
   styleUrls: ['./todo-item.component.css']
 })
 export class TodoItemComponent implements OnInit {
+  @Input() item;
 
   constructor() { }
 
