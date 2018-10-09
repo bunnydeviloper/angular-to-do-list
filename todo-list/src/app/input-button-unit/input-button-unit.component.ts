1bc9fe0e8e1950d0e7b4e9fb7fad4fb274dd34a5
Sophia Mai <phuongmai001@gmail.com>
2018-10-08 22:09:48 -0700

    refactor: inline template for input-button-unit and display on page

--- todo-list/src/app/input-button-unit/input-button-unit.component.ts
+++ todo-list/src/app/input-button-unit/input-button-unit.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-input-button-unit',
-  templateUrl: './input-button-unit.component.html',
+  template: `<p>input-button-unit works!</p>`,
   styleUrls: ['./input-button-unit.component.css']
 })
 export class InputButtonUnitComponent implements OnInit {
