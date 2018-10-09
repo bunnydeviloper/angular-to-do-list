e7e4eafa241f88779355ebf52c553c6d94c5587c
Sophia Mai <phuongmai001@gmail.com>
2018-10-08 22:14:13 -0700

    add new title in sub component

--- todo-list/src/app/input-button-unit/input-button-unit.component.ts
+++ todo-list/src/app/input-button-unit/input-button-unit.component.ts
@@ -2,10 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-input-button-unit',
-  template: `<p>input-button-unit works!</p>`,
+  template: `
+    <p>input-button-unit works!</p>
+    <p>The title is {{ title }}.</p>
+  `,
   styleUrls: ['./input-button-unit.component.css']
 })
 export class InputButtonUnitComponent implements OnInit {
+  title = "Hello World";
 
   constructor() { }
 
