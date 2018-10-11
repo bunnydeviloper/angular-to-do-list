409c25da1731e0c6160123558da3bef3f0cadaf3
Sophia <phuongmai001@gmail.com>
2018-10-10 22:51:41 -0700

    update storage service

--- review/src/app/services/storage.service.ts
+++ review/src/app/services/storage.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
 
+const storageName = "todo-list-storage";
+
 @Injectable({
   providedIn: 'root'
 })
+
+  /*
+export class StorageService {
+  private todoList;
+
+  constructor() {
+    this.todoList = JSON.parse(localStorage.getItem(storageName));
+  }
+
+  // get items
+  get() {
+    return [...this.todoList];
+  }
+
+  // add a new item
+  post(item) {}
+
+  // update an item
+  put(item, changes) {}
+
+  // remove an item
+  destroy(item) {}
+}
+   */
+
+/*
+*/
 export class StorageService {
 
   constructor() { }
+
+  getData(key: string) {
+    JSON.parse(localStorage.getItem(key));
+  }
+
+  setData(key: string, data: any) {
+    localStorage.setItem(key, JSON.stringify(data));
+  }
 }
